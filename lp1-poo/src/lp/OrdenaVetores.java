30d154f7d9b711a37eff0a9533d5dd2c0bb4bc68
Letícia Escobar <leticiaescobaraujo@gmail.com>
2022-08-09 21:14:09 -0300

    Aula-08-08

---
+++ lp1-poo/src/lp/OrdenaVetores.java
@@ -0,0 +1,30 @@
+package lp;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class OrdenaVetores {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        int vetor[] = new int[n];
+        int min = 0;
+        int max = 100;
+        Random rand = new Random();
+
+        System.out.println("\nVETOR DESORDENADO: ");
+        for (int i = 0; i < n; i++) {
+            vetor[i] = rand.nextInt(max - min) + min;
+            System.out.print(vetor[i] + "\t");
+        }
+
+        System.out.println("\n\nVETOR ORDENADO: ");
+        Arrays.sort(vetor);
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + "\t");
+        }
+
+    }
+}
